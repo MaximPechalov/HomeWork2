5972acc455aab5a63f03c6811123764142338cd8
Maxim Pechalov <magaterkolg@gmail.com>
2023-06-14 14:04:50 +0300

    ex3

---
+++ ex3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите номер дня недели: ");
+int day = int.Parse(Console.ReadLine());
+
+if (day > 7)
+{
+    Console.WriteLine("Invalid param");
+    return;
+}
+
+if (day == 6 || day == 7)
+{
+    Console.WriteLine("Сегодня выходной");
+}
+else
+{
+    Console.WriteLine("Сегодня не выходной");
+}
