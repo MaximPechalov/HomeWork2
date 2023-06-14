6c0777675e04e2c12ce3a52c0ec91c7e4ad40e37
Maxim Pechalov <magaterkolg@gmail.com>
2023-06-14 13:50:12 +0300

    ex1

---
+++ ex1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число трёхзначное число: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num < 100 || num > 999)
+{
+    Console.WriteLine("Неверное число");
+    return;
+}
+
+int result = num % 100;
+int result2 = result / 10;
+Console.WriteLine($"Вторая цифра числа: {result2}");
