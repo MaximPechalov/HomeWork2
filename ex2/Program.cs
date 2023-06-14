96f74cc27e05368afd7707112b399ffbbd5b9c9d
Maxim Pechalov <magaterkolg@gmail.com>
2023-06-14 14:00:37 +0300

    ex2

---
+++ ex2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int result = 0;
+
+if (num < 100)
+{
+    Console.WriteLine("Третьего числа нет!");
+}
+
+
+Console.WriteLine(result);
